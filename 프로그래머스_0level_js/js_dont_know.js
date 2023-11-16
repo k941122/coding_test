781ba6f588bda3e516ec0f115fd44ede9cacef8e
k941122 <941122k@naver.com>
2023-11-16 16:51:33 +0900

    js문법모르는 부분_Math.floor

--- 프로그래머스_0level_js/js_dont_know.js
+++ 프로그래머스_0level_js/js_dont_know.js
@@ -17,3 +17,32 @@ function discount (isMember) {
 console.log(isMember(true)); // 1000원
 console.log(isMember(false)); //2000원
 
+/**
+ * Math.floor 매서드 란?
+ * 
+ * Math 메서드 : 수학적인 상수와 함수를 위한 속성과 매서드를 가진 내장객체.
+ * Math는 Number 자료형만 지원한다. 
+ * 
+ * Math.floor(); 는 무엇인가?
+ * 
+ * Math.floor() 함수는 주어진 숫자와 같거나 작은 정수중에서 가장 큰수를 반환 한다.
+ * 
+ * 구문
+ * Math.floor(x);
+ * 
+ * 매개변수(parameter)
+ * x
+ * 
+ * 반환 값
+ * 주어진 수 이하의 가장 큰 정수!
+ * 
+ * floor()은 Math의 정적 메스드 이므로, 사용자가 생성한 Math 객체의 매서드로 호출할수 없고
+ * 항상 Math.floor()를 사용해야 합니다. (Math는 생성자가 아닙니다.)
+ * 
+ * ex) console.log(Math.floor(45.95);) //45
+ * 
+ * 위 예시의 경우 Math.floor 일반괄호 안에 45.95 라는 숫자에서 
+ * 큰 숫자인 45만 인출하며 소수점 자리인 .95는 인출하지 않는다.
+ * 
+ * 
+ */
