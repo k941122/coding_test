828daaa43a44747aeba2d01264642d27643bb3a8
k941122 <941122k@naver.com>
2023-11-16 17:14:21 +0900

    [0level] 나머지 구하기

---
+++ 프로그래머스_0level_js/나머지_구하기.js
@@ -0,0 +1,32 @@
+
+//나머지 구하기
+
+/**
+ * 문제 설명
+ * 
+ * 정수 num1, num2가 매개변수로 주어질 때, 
+ * num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+ * 
+ * 제한사항
+ * - 0 < num1 ≤ 100
+ * - 0 < num2 ≤ 100
+ * 
+ * 입출력 예시
+ * 
+ * num1	num2 result
+ *   3	 2	   1
+ *   10	 5	   0
+ * 
+ * 입출력 예 설명
+ * 
+ * ex1) num1이 3, num2가 2이므로 3을 2로 나눈 나머지 1을 return 합니다.
+ * ex2) num1이 10, num2가 5이므로 10을 5로 나눈 나머지 0을 return 합니다.
+ * 
+ */
+
+function solution (num1,num2) {
+    if(num1>0||num1<=100||num2>0||num2<=100) {
+        console.log('제한 사항과 일치하지 않았습니다. ${num1,num2}를 다시 입력해 주세요.');
+    } let answer = Math.floor(num1%num2);
+    return answer;
+}
