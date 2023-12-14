c34193e31a99afbf63b7a48051456f8651e276f9
k941122 <941122k@naver.com>
2023-12-14 19:24:13 +0900

    [0level] Title: 합성수 찾기_정답 수정

--- 프로그래머스_0level_js/합성수_찾기.js
+++ 프로그래머스_0level_js/합성수_찾기.js
@@ -55,7 +55,9 @@ function solution (n) {
 
     for (let i = 1; i <= n;i++) {
         for (let j =2; j < i; j++ ) {
+            if(i%j === 0){                         
             arr.push(i)
+            }
         }
     }
     return [...new Set(arr)].length;
